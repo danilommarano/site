e545e982840b7e1e3c4da1d8e3ea80072e6adc5c
Danilo Matrangolo Marano <danilo.marano@mac.com.br>
2023-06-19 13:35:08 -0300

    Componentes customizados na página principal

--- app/page.tsx
+++ app/page.tsx
@@ -1,37 +1,48 @@
-import Image from 'next/image'
-import { GitHubIcon, LinkedInIcon } from '@/components/Icons'
+import Image from "next/image";
+import { GitHubIcon, LinkedInIcon } from "@/components/Icons";
+import Section from "@/components/Section";
+import Nav from "@/components/Nav";
+import Paragraph from "@/components/Paragraph";
+import NavList from "@/components/NavList";
+import NavItem from "@/components/NavItem";
+import Logo from "@/components/Logo";
+import Header from "@/components/Header";
 
 export default function Home() {
   return (
     <main className="bg-[#e2e2e2]">
-      <nav className='flex justify-between items-center h-16 w-full px-10 bg-[#e2e2e2]'>
-        <p className='text-xl font-extrabold'>Danilo</p>
-        <ul className='flex gap-6 font-bold'>
-          <li className=''>Home</li>
-          <li className=''>About</li>
-          <li className=''>Projects</li>
-          <li className=''>Contact</li>
-        </ul>
-      </nav>
-      <section className='flex justify-center items-center h-[700px] '>
-        <div className='flex flex-col w-[700px]'>
-          <div className='flex w-full gap-6'>
-            <div className='flex flex-col gap-4'>
-              <h1 className='text-5xl font-extrabold'>Cientista de Dados 👋</h1>
-              <p className=''>Olá, me chamo Danilo Marano. Sou programador apaixonado por estatística vivendo em São Paulo, Brasil. 📍</p>
-              <div className='flex gap-4'>
+      <Nav>
+        <Logo />
+        <NavList>
+          <NavItem>Home</NavItem>
+          <NavItem>About</NavItem>
+          <NavItem>Projects</NavItem>
+          <NavItem>Contact</NavItem>
+        </NavList>
+      </Nav>
+      <Section>
+        <div className="flex flex-col w-[700px]">
+          <div className="flex w-full gap-6">
+            <div className="flex flex-col gap-4">
+              <Header level="1">Cientista de Dados 👋</Header>
+              <Paragraph>
+                Olá, me chamo Danilo Marano. Sou programador apaixonado por
+                estatística vivendo em São Paulo, Brasil. 📍
+              </Paragraph>
+              <div className="flex gap-4">
                 <LinkedInIcon />
                 <GitHubIcon />
               </div>
             </div>
-            <Image src='/../public/MinhaFoto.png' width={400} height={400} />
+            <Image
+              src="/../public/MinhaFoto.png"
+              width={400}
+              height={400}
+              alt="Foto do Danilo"
+            />
           </div>
         </div>
-        
-      </section>
-      
-
-
+      </Section>
     </main>
-  )
+  );
 }
